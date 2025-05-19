3a0b08df9e4f439042e39ad672d6c0187ca19dc8
sardorml <sardormamadaliyev@gmail.com>
2025-05-19 19:11:42 +0500

    setup shadcn

---
+++ week-6/shadcn-setup/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
